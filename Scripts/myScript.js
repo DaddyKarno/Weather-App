72a17db9087972730081bcc46948e9affba0cac3
Ivan Karnoushenko <karnoushenko24@gmail.com>
2023-11-05 10:26:59 +0300

    feature(logic):WA:2.output other date, add block catch

--- Scripts/myScript.js
+++ Scripts/myScript.js
@@ -1,4 +1,11 @@
 const cityName = document.querySelector(".searchBarInput");
+setInterval(() => {
+  let timeInCity = new Date();
+  document.querySelector(".timeStyle").textContent =
+    timeInCity.toLocaleTimeString();
+  document.querySelector(".dateStyle").textContent =
+    timeInCity.toLocaleDateString();
+}, 1000);
 document.getElementById("inputSearch").onclick = function () {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=4e2a28904642308191f507da7136a241`
@@ -7,17 +14,39 @@ document.getElementById("inputSearch").onclick = function () {
       return resp.json();
     })
     .then(function (data) {
-      console.log(data);
-      document.querySelector(".cityStyle").textContent = data.name;
-      document.querySelector(".celsius").innerHTML =
-        Math.round(data.main.temp - 273) + "°C";
-      document.querySelector(".feelsCelsius").innerHTML =
-        "feels like: " + Math.round(data.main.feels_like - 273) + "°C";
-      document.querySelector(".weatherName").textContent =
-        data.weather[0]["description"];
-      document.querySelector(
-        ".weatherImage"
-      ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png" width="300" height="300">`;
+      try {
+        document.querySelector(".cityStyle").textContent = data.name;
+        document.querySelector(".celsius").textContent =
+          roundCelsius(data.main.temp) + "°C";
+        document.querySelector(".feelsCelsius").textContent =
+          "feels like: " + roundCelsius(data.main.feels_like) + "°C";
+        document.querySelector(".weatherName").textContent =
+          data.weather[0]["description"];
+        document.querySelector(
+          ".weatherImage"
+        ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png" width="600" height="600">`;
+        document.querySelector(".sunriseTimeStyle").textContent =
+          `Sunrise\n` + suntime(data.sys.sunrise * 1000);
+        document.querySelector(".sunsetTimeStyle").textContent =
+          `Sunset\n` + suntime(data.sys.sunset * 1000);
+        document.querySelector(".Humidity").textContent =
+          data.main.humidity + " %";
+        document.querySelector(".Pressure").textContent =
+          data.main.pressure + "hPa";
+        document.querySelector(".windSpeed").textContent =
+          data.wind.speed + "km/h";
+      } catch {
+        alert(data.cod + " " + data.message);
+      }
     });
+
   cityName.value = "";
 };
+function roundCelsius(сelcius) {
+  let a = Math.round(сelcius - 273);
+  return a;
+}
+function suntime(suntime) {
+  let sunrise = new Date(suntime);
+  return sunrise.toLocaleTimeString();
+}
